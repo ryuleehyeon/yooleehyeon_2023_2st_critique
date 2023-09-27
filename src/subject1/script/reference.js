e3fe3f04da65c424c865ffea8b1028296546a4c8
ryuleehyeon <139140606+ryuleehyeon@users.noreply.github.com>
2023-09-27 14:04:43 +0900

    critique_927_2

--- src/subject1/script/reference.js
+++ src/subject1/script/reference.js
@@ -1,9 +1,24 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
+    author: "knu",
+    title: "KNU emotion dictionary ",
+    year: "2020",
+    sourceTitle: "KNU emotion dictionary",
+    link: "http://dilab.kunsan.ac.kr/knusl.html",
+  },
+  {
+    author: "knu",
+    title: "KNU emotion dictionary ",
+    year: "2021",
+    sourceTitle: "KNU emotion dictionary",
+    link: "http://dilab.kunsan.ac.kr/knu/knu.html",
+  },
+  {
+    author: "Ju Ri Ae Lee",
+    title:
+      "Analysis on Sight Translation Assisted by Automatic Speech Recognition-Focused on the Use of Clovanote and Fluency-",
+    year: "2022",
     sourceTitle: "",
-    link: "",
+    link: "https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE11252118",
   },
 ];
