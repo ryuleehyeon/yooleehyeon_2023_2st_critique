fa2dcd61f78dd7b87745f7607b65bd0b65d4b6b2
ryuleehyeon <139140606+ryuleehyeon@users.noreply.github.com>
2023-10-01 19:14:21 +0900

    critique_101_1

--- src/subject2/script/reference.js
+++ src/subject2/script/reference.js
@@ -1,26 +1,5 @@
 const reference = [
   {
-    author: "kakao",
-    title: "The beginning of gift giving",
-    year: "2010",
-    sourceTitle: "",
-    link: "https://https://www.kakaocorp.com/page/detail/5216.com/@ilon_a",
-  },
-  {
-    author: "team",
-    title: "Happy X-Day | Gift Recommendation Website",
-    year: "2022",
-    sourceTitle: "",
-    link: "https://www.behance.net/gallery/159351599/Happy-X-Day-Gift-Recommendation-Website?tracking_source=search_projects|gift+ui&",
-  },
-  {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
-  },
-  {
     author: "",
     title: "",
     year: "",
